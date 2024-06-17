b924bbf37a29bde79becd280c1eeaccf65c2790a
Uday Tiwari <udaytiwari360@outlook.com>
2024-06-17 12:45:32 +0530

    Get, Set and Go!

---
+++ README.md
@@ -0,0 +1,20 @@
+# Conway's Game of Life
+
+### RULES -
+- Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+- Any live cell with two or three live neighbours lives on to the next generation.
+- Any live cell with more than three live neighbours dies, as if by overpopulation.
+- Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+---
+### Running the Game!
+- Windows =>
+    ``` powershell
+    PS> py .\gol.py
+    ```
+- *nix =>   
+    ``` console
+    $ ./gol.py
+    ```
+---
+### Example =>
+![Game](./game.jpg)
---
+++ gol.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+""" Conway's Game of Life Implementation in Python 3.12 """
+
+# Created: 6/16/2024, 8:10 PM IST 
+
+import os
+import time as T
+
+def console_clear() -> None | OSError:
+    os.system("clear")
+
+class Board:
+    
+    GOLBoard:[[str]] = []
+    new_Board:[[str]] = []
+
+    ROWS = 0
+    COLUMNS = 0
+    TPS = 15
+    GENERATIONS = 100
+
+    def __init__(self, board: [[str]], rows: int, column: int) -> None:
+
+        self.ROWS = rows
+        self.COLUMNS = column
+        self.GOLBoard = board
+        self.new_Board = [["."]*self.COLUMNS for _ in range(self.ROWS)]
+
+    def get_nebr(self, row_0: int, col_0: int) -> int:
+        nebr: int = 0
+        row: int = 0
+        col: int = 0
+
+        for dr in range(-1, 2):
+            for dc in range(-1, 2):
+                if (dr != 0 or dc != 0):
+                    row = (row_0 + dr) % self.ROWS
+                    col = (col_0 + dc) % self.COLUMNS
+        
+                    if (self.GOLBoard[row][col] == "#"):
+                        nebr += 1
+
+        return nebr
+
+    def next_gen(self) -> None:
+        N: int = 0
+
+        for r in range(self.ROWS):
+            for c in range(self.COLUMNS):
+                N = self.get_nebr(r, c)                
+                if (N < 2 or N > 3):
+                    self.new_Board[r][c] = "."
+                elif (N == 3 and self.GOLBoard[r][c] == "."):
+                    self.new_Board[r][c] = "#"
+                else:
+                    self.new_Board[r][c] = self.GOLBoard[r][c]
+            
+        self.GOLBoard, self.new_Board = self.new_Board, self.GOLBoard
+        self.render_board()
+
+    def render_board(self) -> None:
+        console_clear()
+
+        for r in range(self.ROWS):
+            for c in range(self.COLUMNS):
+                print(self.GOLBoard[r][c], end=' ')
+            print()
+
+# Seed Configuration
+b: [[str]] = [
+    ['.', '#', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '#', '.', '.', '.', '.', '.'],
+    ['#', '#', '#', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.'],
+]
+
+B = Board(b, len(b), len(b[0]))
+
+for i in range(B.GENERATIONS):
+    B.next_gen()
+    T.sleep(1 / B.TPS)
